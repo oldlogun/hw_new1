c285c797e8249d792ce2a4448fa83dbda81d259f
Vitaly <Vitaly@DESKTOP-0IE18EK>
2024-05-22 17:30:08 +0300

    Добавьте файлы проекта.

---
+++ hw_new1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_new1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabet = 'A';
+            byte daysOfWeek = 7;
+            sbyte temperature = 0;
+            int moneyOutWallet = -100;
+            uint moneyInWallet = 100;
+            long nationalDebt = 1000000000000000000;
+            float businessShare = 1/5f;
+            string name = "Peter";
+            bool isOpen = false;
+            double backpackWeight = 0.56464;
+        }
+    }
+}
